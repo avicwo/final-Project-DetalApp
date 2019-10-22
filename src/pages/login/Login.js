15566e9cc674f1f785d70e0f303dfcd64604b772
avi cohen <avicwo@gmail.com>
2019-10-22 15:00:26 +0300

    include login and logout validated, set nav in components

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './Login.css';
-import { Alert,Nav, Navbar, Form, Button } from 'react-bootstrap';
+import { Alert, Nav, Navbar, Form, Button } from 'react-bootstrap';
 import { Redirect } from 'react-router-dom'
 import Parse from 'parse';
+// import { User } from '../../data-model/user-md';
+//need to amend this part to take from the datamodel
 import { User } from './User-data-model';
-// import { User } from '../data-model/User';
-
 
 
 
@@ -15,12 +15,36 @@ class Login extends React.Component {
         super(props);
         this.state = {
             invalidLogin: false,
-            successLogin: false
+            successLogin: false,           
+            //  successLogin: true
+
+
         }
         this.emailInput = React.createRef();
         this.pwdInput = React.createRef();
         this.login = this.login.bind(this);
+        this.handleKeyPress = this.handleKeyPress.bind(this)
     }
+
+    handleKeyPress(e) {
+        if (e.key === "Enter") {
+            Parse.User.logIn(this.emailInput.current.value, this.pwdInput.current.value).then(user => {
+                // Do stuff after successful login
+                console.log('Logged in user', user);
+
+                // calling App's callback function
+                // this.props.handleLogin(new User(user));
+                this.props.handleLogin(new User(user));
+                this.setState({ successLogin: true });
+                console.log(this.state.successLogin)
+
+            }).catch(error => {
+                console.error('Error while logging in user', error);
+                this.setState({ invalidLogin: true });
+            })
+        };
+    }
+    
     login() {
 
         Parse.User.logIn(this.emailInput.current.value, this.pwdInput.current.value).then(user => {
@@ -30,13 +54,12 @@ class Login extends React.Component {
             // calling App's callback function
             // this.props.handleLogin(new User(user));
             this.props.handleLogin(new User(user));
-
-            this.setState({successLogin: true});
-            console.log(this.state.successLogin )
+            this.setState({ successLogin: true });
+            console.log(this.state.successLogin)
 
         }).catch(error => {
             console.error('Error while logging in user', error);
-            this.setState({invalidLogin: true});
+            this.setState({ invalidLogin: true });
         });
     }
 
@@ -46,43 +69,39 @@ class Login extends React.Component {
             return <Redirect to="/adminPage" />
         }
 
-
         return (
             <div>
                 <Navbar bg="primary" variant="dark">
                     <Navbar.Brand href="/">דנטלפורם</Navbar.Brand>
                     <Nav className="mr-auto">
-                        {/* <Nav.Link href="#about">אודות</Nav.Link> */}
                     </Nav>
                 </Navbar>
 
                 <div className="login">
-                <h1>הכנס למערכת</h1>
-                 <p>or <a href="#/signup">צור משתמש חדש</a></p>
-                <Alert variant="danger" show={this.state.invalidLogin}>
-                    Invalid email or password!
+                    <h1>הכנס למערכת</h1>
+                    <p>or <a href="#/signup">צור משתמש חדש</a></p>
+                    <Alert variant="danger" show={this.state.invalidLogin}>
+                        Invalid email or password!
                 </Alert>
-                <Form>
-                    <Form.Group controlId="formBasicEmail">
-                        <Form.Label>כתובת אימייל</Form.Label>
-                        <Form.Control ref={this.emailInput} type="email" placeholder="Enter email" />
-                        <Form.Text className="text-muted">
-                            We'll never share your email with anyone else.
+                    <Form>
+                        <Form.Group controlId="formBasicEmail">
+                            <Form.Label>כתובת אימייל</Form.Label>
+                            <Form.Control ref={this.emailInput} type="email" placeholder="Enter email" />
+                            <Form.Text className="text-muted">
+                                We'll never share your email with anyone else.
                         </Form.Text>
-                    </Form.Group>
+                        </Form.Group>
 
-                    <Form.Group controlId="formBasicPassword">
-                        <Form.Label>סיסמא   </Form.Label>
-                        <Form.Control ref={this.pwdInput} type="password" placeholder="Password" />
-                    </Form.Group>
-                    <Button variant="success" type="button" block onClick={this.login}>
-                        Login
+                        <Form.Group controlId="formBasicPassword">
+                            <Form.Label>סיסמא   </Form.Label>
+                            <Form.Control ref={this.pwdInput} type="password" placeholder="Password" onKeyPress={this.handleKeyPress} />
+                        </Form.Group>
+                        <Button variant="success" type="button" block onClick={this.login} onKeyPress={this.handleKeyPress}>
+                            Login
                     </Button>
-                </Form>
+                    </Form>
 
-            </div>
-
-         
+                </div>
             </div>
 
         );
