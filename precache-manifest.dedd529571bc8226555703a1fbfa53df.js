878d4ae8df0948e674d11c688d9264b6aa15b13e
avi cohen <avicwo@gmail.com>
2019-10-27 11:49:51 +0200

    Updates

---
+++ precache-manifest.dedd529571bc8226555703a1fbfa53df.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c17e1506e84a69229f11f86bca9a82c",
+    "url": "/final-Project-DetalApp/index.html"
+  },
+  {
+    "revision": "f8f91704a285616552e9",
+    "url": "/final-Project-DetalApp/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "d850fc18f10b3852d0bc",
+    "url": "/final-Project-DetalApp/static/css/main.2302d096.chunk.css"
+  },
+  {
+    "revision": "f8f91704a285616552e9",
+    "url": "/final-Project-DetalApp/static/js/2.4152393c.chunk.js"
+  },
+  {
+    "revision": "d850fc18f10b3852d0bc",
+    "url": "/final-Project-DetalApp/static/js/main.64a2cc03.chunk.js"
+  },
+  {
+    "revision": "652c14fb9e2f49368c9b",
+    "url": "/final-Project-DetalApp/static/js/runtime~main.bb114e1e.js"
+  },
+  {
+    "revision": "ce8ba056fc91d21407c84e9d0533b093",
+    "url": "/final-Project-DetalApp/static/media/capture2.ce8ba056.PNG"
+  },
+  {
+    "revision": "faf6ce51a9af78b25e4d9c813236b46a",
+    "url": "/final-Project-DetalApp/static/media/landingpage1.faf6ce51.jpg"
+  }
+]);
